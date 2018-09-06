34d1537e07582f07f545e5c13254a49f908a71d2
DESKTOP-STF9L29\Sheng-Wei Lin <a182820998@gmail.com>
2018-09-06 18:06:38 +0800

    [開發] imgur api sign up [開發] 加入 dotenv

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App';
+import dotenv from 'dotenv';
 
-Vue.config.productionTip = false
+// in vue runtime all .env variables' name must start with "VUE_APP_"
+dotenv.config();
 
 new Vue({
-  render: h => h(App)
-}).$mount('#app')
+   render: h => h(App),
+}).$mount('#app');
