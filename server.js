364355e1585c49fe9133152cf9208f21112f02c5
shenglin-alex <a182820998@gmail.com>
2019-07-18 23:45:41 +0800

    'PORT'

--- server.js
+++ server.js
@@ -12,5 +12,5 @@ app.use(history({
 }));
 app.use(staticFileMiddleware); // second call to history mode redirect to index.html and resolve it
 
-const port = process.env.SERVER_PORT || 8080;
+const port = process.env.PORT || 8080;
 app.listen(port, '0.0.0.0');
